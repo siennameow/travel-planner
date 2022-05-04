8dec6e6a4c478471c0882a3ac9d305e1ef71fe4a
walterunderwood <walonsounderwood@gmail.com>
2022-05-04 11:37:02 -0700

    Creating travellers and trips Routes structure

---
+++ Develop/routes/api/travelerRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { } = require('../../model/index');
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
---
+++ Develop/routes/api/tripRoute.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const { } = require('../../model/index');
+
+
+router.post('/', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
